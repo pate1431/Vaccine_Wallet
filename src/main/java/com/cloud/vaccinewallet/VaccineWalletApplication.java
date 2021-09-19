920d4bb5e113099607a3499a6915513527cae447
pate1431 <devsnehalpatel@gmail.com>
2021-09-19 13:21:08 -0400

    initial commit

---
+++ src/main/java/com/cloud/vaccinewallet/VaccineWalletApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.vaccinewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccineWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloud/vaccinewallet/VaccineWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.vaccinewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

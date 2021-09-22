1313393de12045ad89f9a17384d4e8313f66173c
pate1431 <devsnehalpatel@gmail.com>
2021-09-22 19:52:41 -0400

    securities and Database added

---
+++ src/main/java/com/cloud/vaccinewallet/config/UserDetailsServiceImpl.java
@@ -0,0 +1,48 @@
+package com.cloud.vaccinewallet.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cloud.vaccinewallet.beans.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cloud.vaccinewallet.beans.Role;
+import com.cloud.vaccinewallet.repositories.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // Find the user based on the user name
+        com.cloud.vaccinewallet.beans.User user = userRepository.findByUsername(username);
+
+        // If the user doesn't exist throw an exception
+        if (user == null) {
+            System.out.println("User not found:" + username);
+            throw new UsernameNotFoundException("User " + username + " was not found in the database");
+        }
+
+        // Change the list of the user's roles into a list of GrantedAuthority
+        List<GrantedAuthority> grantList= new ArrayList<GrantedAuthority>();
+        for (Role role: user.getRoles()) {
+            grantList.add(new SimpleGrantedAuthority(role.getRolename()));
+        }
+
+        // Create a user based on the information above.
+        UserDetails userDetails= (UserDetails) new User(user.getUsername(), user.getEncryptedPassword(), grantList);
+        return userDetails;
+    }
+
+}
---
+++ src/main/java/com/cloud/vaccinewallet/controller/HomeController.java
@@ -0,0 +1,81 @@
+package com.cloud.vaccinewallet.controller;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.cloud.vaccinewallet.beans.User;
+import com.cloud.vaccinewallet.repositories.UserRepository;
+import com.cloud.vaccinewallet.repositories.RoleRepository;
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class HomeController {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
+    private String encodePassword(String password) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(password);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String userIndex(Model model, Authentication authentication) {
+
+        String name = authentication.getName();
+
+        List<String> roles = new ArrayList<String>();
+
+        for (GrantedAuthority ga: authentication.getAuthorities()) {
+            roles.add(ga.getAuthority());
+        }
+        model.addAttribute("name", name);
+        model.addAttribute("roles", roles);
+
+        return "user/displayHotel";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "/error/access-denied";
+    }
+
+    @GetMapping("/register")
+    public String goRegistration() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String doRegistration(Model model,@RequestParam String username, @RequestParam String password) {
+
+
+        User user= new User(username, encodePassword(password), Byte.valueOf("1"));
+        user.getRoles().add(roleRepository.findByRolename("ROLE_USER"));
+
+        userRepository.save(user);
+        model.addAttribute("userList",userRepository.findAll());
+        return "redirect:/";
+    }
+}

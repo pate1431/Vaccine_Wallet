6dd64c285b4e818eb44f6c087826bd7c8b640e12
pate1431 <devsnehalpatel@gmail.com>
2021-10-25 13:10:25 -0400

    securities changed

--- src/main/java/com/cloud/vaccinewallet/config/SecurityConfig.java
+++ src/main/java/com/cloud/vaccinewallet/config/SecurityConfig.java
@@ -1,58 +1,59 @@
 package com.cloud.vaccinewallet.config;
 
+import com.cloud.vaccinewallet.beans.Roles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration
 @EnableWebSecurity
-@Order(2)
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private LoggingAccessDeniedHandler accessDeniedHandler;
-
-    @Autowired
-    UserDetailsServiceImpl userDetailsService;
+    private final LoggingAccessDeniedHandler accessDeniedHandler;
+    private final UserDetailsServiceImpl userDetailsService;
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override protected void configure(HttpSecurity http) throws Exception {
-
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-
-        http.authorizeRequests()
-                .antMatchers("/user/**").hasRole("USER")
-                .antMatchers(HttpMethod.POST, "/register").permitAll()
-                .antMatchers("/","/css/**","static/**","/image/**", "/js/**", "/**")
-                .permitAll()
-                .antMatchers("/h2-console/**").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and().formLogin()
-                .loginPage("/login").permitAll()
-                .and().logout()
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login?logout").permitAll()
-                .and()
-                .exceptionHandling()
-                .accessDeniedHandler(accessDeniedHandler);
+    @Bean
+    public AuthenticationSuccessHandler customeSuccessHandler() {
+        return new CustomeSuccessHandler();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());}
+
+    public SecurityConfig(LoggingAccessDeniedHandler accessDeniedHandler, UserDetailsServiceImpl userDetailsService) {
+        this.accessDeniedHandler = accessDeniedHandler;
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().headers().frameOptions().disable().and().authorizeRequests()
+                .antMatchers("/", "/css/**", "static/**", "/image/**", "/js/**", "/**").permitAll()
+                .antMatchers("/admin/**").hasRole(Roles.ADMIN.name())
+                .antMatchers(("/user/**")).hasRole(Roles.USER.name()).anyRequest().authenticated().
+                and().formLogin().loginPage("/login")
+                .loginProcessingUrl("/login").successHandler(customeSuccessHandler()).permitAll().and().logout().invalidateHttpSession(true).clearAuthentication(true).logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout").permitAll()
+                .and().exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
 }
